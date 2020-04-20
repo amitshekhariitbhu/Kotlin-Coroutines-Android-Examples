0830d1b6528f56815a5b44d41bd8a3d88c4e5ddc
amitshekhar <amit.shekhar.iitbhu@gmail.com>
2020-04-20 21:34:38 +0530

    Add utils

---
+++ app/src/main/java/com/mindorks/example/coroutines/utils/Resource.kt
@@ -0,0 +1,21 @@
+package com.mindorks.example.coroutines.utils
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    companion object {
+
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/mindorks/example/coroutines/utils/Status.kt
@@ -0,0 +1,7 @@
+package com.mindorks.example.coroutines.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
